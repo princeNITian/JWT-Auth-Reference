d00e7867416b9902946b680b64650aba495449d4
Prince Raj <princeraj427@gmail.com>
2021-10-04 20:03:41 +0530

    register logic implemented

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 require('./config/database').connect();
 const express = require('express');
-const user = require('./model/user');
+const User = require('./model/user');
 
 const app = express();
 
@@ -10,17 +10,62 @@ app.use(express.json());
 // Logic goes here
 
 // Register here
-app.post('/register',(req,res) => {
+app.post('/register', async (req,res) => {
+    // our register logic goes here
+    try {
+        // Get user Input
+        const { first_name, last_name, email, password } = req.body;
 
-})
+        // validate user Input
+        if(!(email && password && first_name && last_name)){
+            res.status(400).send('All Inputs are required!');
+        }
+
+        // check if user already exist
+        // Validate if user exist in our database
+        const oldUser = await User.findOne({email});
+
+        if(oldUser){
+            res.status(409).send('User already exist! Please Login.');
+        }
+
+        // encrypt user password
+        encryptedPass = await bcrypt.hash(password,10);
+
+        // Create user in our database
+        const user = await User.create({
+            first_name,
+            last_name,
+            email: email.toLowercase(), // sanitize: convert email to lowercase
+            password: encryptedPass
+        });
+
+        // Create token
+        const token = jwt.token(
+            { user_id: user._id, email },
+            process.env.TOKEN_KEY,
+            {
+                expiresIn: "2h"
+            }
+        );
+        // save user token
+        user.token = token;
+
+        // return new user
+        res.status(401).json(user);
+
+
+    }
+    catch(err){
+        console.log(err);
+    }
+    // Our register logic ends 
+});
 
 // Login here
 app.post('/login',(req,res) => {
 
 })
-
-
-
 
 
 module.exports = app;
