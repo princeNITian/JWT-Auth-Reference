d26a04579642676b0a79f354c6eb4827fa197023
Prince Raj <princeraj427@gmail.com>
2021-10-04 19:20:16 +0530

    Created user model and routes

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 require('dotenv').config();
 require('./config/database').connect();
 const express = require('express');
+const user = require('./model/user');
 
 const app = express();
 
 app.use(express.json());
 
 // Logic goes here
+
+// Register here
+app.post('/register',(req,res) => {
+
+})
+
+// Login here
+app.post('/login',(req,res) => {
+
+})
+
+
+
+
 
 module.exports = app;
